1bf319340232ccd60b73e06ea8a3000e9a655be3
Cecilia Nistal <cecilia.nistal@centific.com>
2024-04-30 22:44:51 -0300

    Added Checklist Datahandler

--- Apps.Trello/Models/Requests/Checklist/SearchItemRequest.cs
+++ Apps.Trello/Models/Requests/Checklist/SearchItemRequest.cs
@@ -1,10 +1,13 @@
-﻿using Blackbird.Applications.Sdk.Common;
+﻿using Apps.Trello.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Trello.Models.Requests.Checklist
 {
     public class SearchItemRequest
     {
         [Display("Checklist ID")]
+        [DataSource(typeof(ChecklistDataHandler))]
         public string ChecklistID { get; set; }
 
         [Display("Check item name")]
