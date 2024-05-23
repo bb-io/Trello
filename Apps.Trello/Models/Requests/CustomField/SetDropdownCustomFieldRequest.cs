9a05f8694ce0aee8c94470b4f8f655aca736d7da
Cecilia Nistal <cecilia.nistal@centific.com>
2024-05-23 20:23:42 -0300

    Added Custom field actinos

---
+++ Apps.Trello/Models/Requests/CustomField/SetDropdownCustomFieldRequest.cs
@@ -0,0 +1,16 @@
+﻿using Apps.Trello.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+using Manatee.Trello;
+
+namespace Apps.Trello.Models.Requests.CustomField
+{
+    public class SetDropdownCustomFieldRequest
+    {
+        [Display("Custom field ID"), DataSource(typeof(DropdownCustomFieldDataHandler))]
+        public string CustomFieldId { get; set; }
+
+        [Display("Dropdown option ID"), DataSource(typeof(DropdownOptionDataHandler))]
+        public string OptionId { get; set; }
+    }
+}
