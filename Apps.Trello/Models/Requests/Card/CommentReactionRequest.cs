cd37c88fb2acf0ca88214ce845c8dec9a49ea2d6
ce-nistal <cecilia.nistal@blackbird.io>
2024-09-18 22:36:50 -0300

    Add reaction to comment

---
+++ Apps.Trello/Models/Requests/Card/CommentReactionRequest.cs
@@ -0,0 +1,16 @@
+﻿using Apps.Trello.DataSourceHandlers.Static;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dictionaries;
+using Manatee.Trello;
+
+namespace Apps.Trello.Models.Requests.Card
+{
+    public class CommentReactionRequest : CardRequest
+    {
+        [Display("Text comment")]
+        public string textComment { get; set; }
+
+        [StaticDataSource(typeof(EmojiDataHandler))]
+        public string Reaction { get; set; }
+    }
+}
