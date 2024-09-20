2bffc375c4d80139f6de70c1d2d6fa8d32fc0e3a
ce-nistal <cecilia.nistal@blackbird.io>
2024-09-20 11:21:08 -0300

    Added Search cards and Find card actions Added comments and attachments to Get card output

---
+++ Apps.Trello/Models/Responses/Card/GetCardResponse.cs
@@ -0,0 +1,54 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using Manatee.Trello;
+
+namespace Apps.Trello.Models.Responses.Card
+{
+    public class GetCardResponse
+    {
+        [Display("Card ID")]
+        public string ID { get; set; }
+
+        public string Url { get; set; }
+
+        [Display("Card name")]
+        public string Name { get; set; }
+
+        public List<ICheckList> Checklists { get; set; }
+
+        public List<string> Comments { get; set; }
+
+        public List<IAttachment> Attachments { get; set; }
+
+        [Display("List name")]
+        public string ListName { get; set; }
+
+        [Display("List ID")]
+        public string ListID { get; set; }
+
+        public int Position { get; set; }
+
+        public string Description { get; set; }
+
+        [Display("Creation date")]
+        public DateTime CreationDate {get; set; }
+
+        [Display("Last activity date")]
+        public DateTime LastActivity { get; set; }
+
+        public GetCardResponse(ICard card) 
+        {
+            ID = card.Id; 
+            Url = card.Url;
+            Name = card.Name;
+            Checklists = card.CheckLists is null ? new List<ICheckList>() : card.CheckLists.ToList();
+            Comments = card.Comments is null ? new List<string>() : card.Comments.Select(x => x.Data.Text).ToList();
+            Attachments = card.Attachments is null ? new List<IAttachment>() : card.Attachments.ToList();
+            ListName = card.List.Name;
+            ListID = card.List.Id;
+            Description = card.Description;
+            CreationDate = card.CreationDate;
+            LastActivity = DateTime.Now;
+        }
+
+    }
+}
